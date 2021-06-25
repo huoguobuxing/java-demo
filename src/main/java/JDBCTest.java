715320fe85aed5c6dc498bb769cf46969325f18a
zhenli.liu <zhenli.liu@prnasia.com>
2021-06-25 17:24:14 +0800

    测试了多种获取连接的方法，主要是Driver，DriverManager，DataSource，PoolDataSource

--- src/main/java/JDBCTest.java
+++ src/main/java/JDBCTest.java
@@ -1,3 +1,8 @@
+import org.h2.Driver;
+import org.h2.jdbc.JdbcConnection;
+import org.h2.jdbcx.JdbcDataSource;
+
+import javax.sql.PooledConnection;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
@@ -11,7 +16,7 @@ public class JDBCTest {
     static String user = "";
     static String password = "";
 
-    // 创建一个H2数据库，填充一些数据
+    // 创建一个H2数据库，填充一些数据，用于测试
     private static void init() {
         try {
             InputStream resourceAsStream = JDBCTest.class.getClassLoader().getResourceAsStream("jdbc.properties");
@@ -35,9 +40,7 @@ public class JDBCTest {
             stmt.executeUpdate("INSERT INTO USER_INFO VALUES('" + UUID.randomUUID() + "','玄武','男')");
             stmt.executeUpdate("INSERT INTO USER_INFO VALUES('" + UUID.randomUUID() + "','苍狼','男')");
 
-            //释放资源
-            stmt.close();
-            stmt = null;
+            //释放资源 这里不能释放资源，对于内存数据库，一旦链接关闭，内存数据库就不存在了
             conn.close();
             conn = null;
 
@@ -47,21 +50,76 @@ public class JDBCTest {
         System.out.println("初始化完成");
     }
 
-    public static void test1() throws ClassNotFoundException, IOException {
-        Class<?> aClass = Class.forName(url);
+    // 测试使用 DriverManager 获取连接
+    private static void test1() throws SQLException {
+        Connection conn = DriverManager.getConnection(url, user, password);
+        System.out.println(conn instanceof JdbcConnection);
+        System.out.println(conn.isClosed());
+        PreparedStatement preparedStatement = conn.prepareStatement("select * from USER_INFO");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+        conn.close();
+        System.out.println(conn.isClosed());
     }
 
-    public static void test2() throws SQLException, ClassNotFoundException {
-        Connection conn = DriverManager.getConnection(url, user, password);
-        Statement statement = conn.createStatement();
-        System.out.println(statement);
+    // 测试使用 DataSource 获取连接
+    private static void test2() throws SQLException {
+        JdbcDataSource jdbcDataSource = new JdbcDataSource();
+        jdbcDataSource.setURL(url);
+        jdbcDataSource.setUser(user);
+        jdbcDataSource.setPassword(password);
+        Connection conn = jdbcDataSource.getConnection();
+        System.out.println(conn instanceof JdbcConnection);
+        PreparedStatement preparedStatement = conn.prepareStatement("select * from USER_INFO");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+        conn.close();
+        System.out.println(conn.isClosed());
     }
+
+    // 测试使用 PooledDataSource 获取连接
+    private static void test3() throws SQLException {
+        JdbcDataSource jdbcDataSource = new JdbcDataSource();
+        jdbcDataSource.setURL(url);
+        jdbcDataSource.setUser(user);
+        jdbcDataSource.setPassword(password);
+        PooledConnection conn = jdbcDataSource.getPooledConnection();
+        PreparedStatement preparedStatement = conn.getConnection().prepareStatement("select * from USER_INFO");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+        // poolConnection如果关闭，conn.getConnection()将返回null
+        conn.getConnection().close();
+        System.out.println(conn.getConnection().isClosed());
+    }
+
+    // 使用 Driver 直接获取连接
+    private static void test4() throws SQLException, IOException {
+        Driver load = Driver.load();
+
+        InputStream resourceAsStream = JDBCTest.class.getClassLoader().getResourceAsStream("jdbc.properties");
+        Properties properties = new Properties();
+        properties.load(resourceAsStream);
+
+        Connection connect = load.connect(url, properties);
+        System.out.println(connect.isClosed());
+        connect.close();
+        System.out.println(connect.isClosed());
+    }
+
 
     public static void main(String[] args) {
         init();
         try {
 //            test1();
 //            test2();
+//            test3();
+            test4();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
